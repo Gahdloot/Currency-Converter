9b1e9e345c9b26ecddaac4d79813ee7fe08d9a17
gahdloot <davidlearn27@gmail.com>
2022-07-27 22:55:18 +0100

    Initiating test case and renaming file

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from Level_1 import converter
+
+class test_currency(unittest.TestCase):
+    def test_figures(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
